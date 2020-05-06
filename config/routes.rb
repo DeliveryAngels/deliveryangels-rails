cd2c0d41c3ac9e6d8b4f1bb762738f3b4f096221
Jon Wood <jon@blankpad.net>
2020-05-06 22:32:37 +0000

    Add additional attributes at signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :angels
+  devise_for :angels, controllers: {
+    registrations: "angels/registrations",
+  }
   devise_for :users, controllers: {
     sessions: "users/sessions",
   }
