cba9575809d79cfb50bb1eb7e65231365f9f89dd
Jon Wood <jon@blankpad.net>
2020-05-06 21:24:07 +0000

    Redirect logging in angels to deliveries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    return deliveries_path if resource.is_a?(Angel)
+
+    super
+  end
 end
