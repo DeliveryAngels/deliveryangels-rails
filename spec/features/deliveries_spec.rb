b7d6b10652530b1191a33371aabf91344d588737
Jon Wood <jon@blankpad.net>
2020-05-11 20:53:48 +0000

    Super basic listing of assigned deliveries

--- spec/features/deliveries_spec.rb
+++ spec/features/deliveries_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe "an angel who wants to deliver food to people" do
-  fixtures :angels
+  fixtures :angels, :users
 
   context "when not logged in" do
     it "sends them to the log in page" do
@@ -13,19 +13,24 @@ describe "an angel who wants to deliver food to people" do
   end
 
   context "when logged in" do
-    let!(:user) { angels(:bob) }
+    let!(:angel) { angels(:bob) }
+    let!(:user) { users(:user_with_address) }
 
     before do
+      user.orders.create!(angel: angel, address: user.address)
+
       visit "/angels/sign_in"
 
-      fill_in "Email", with: user.email
+      fill_in "Email", with: angel.email
       fill_in "Password", with: "password"
       click_button("Log in")
     end
 
-    it "welcomes them" do
-      expect(page).to have_current_path(deliveries_path)
-      expect(page).to have_content("Welcome, #{user.first_name}")
+    it "includes a list of orders that have been accepted" do
+      expect(page).to have_content("Request 1")
+      expect(page).to have_content("Delivery for #{user.first_name}")
     end
+
+    it "includes orders that need someone to deliver them"
   end
 end
