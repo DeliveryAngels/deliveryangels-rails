2fd6c7512376aede0a7cde026946254dd6c3cc47
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-05-26 10:04:23 +0100

    Convert the copied requires
    Used the railties requires from rails/all, and that has upset Rubocop.
    Hush now, Rubocop.

--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,15 @@
 
 require_relative "boot"
 
-%w(
-  active_record/railtie
-  active_storage/engine
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  action_mailbox/engine
-  action_text/engine
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
